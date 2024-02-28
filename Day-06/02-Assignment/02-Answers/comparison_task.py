ef138f2c902c2fbf842fe87030c614e8b8d485a6
DORCAS ADEBAYO <adesdorcas@gmail.com>
2024-02-28 04:06:50 +0000

    new changes

--- Day-06/02-Assignment/02-Answers/comparison_task.py
+++ Day-06/02-Assignment/02-Answers/comparison_task.py
@@ -1,5 +1,16 @@
 a = 24
 b = 6
 
-a > b
-b < a
+less_than = b < a
+greater_than = a > b
+less_than_or_equal = b <= a
+greater_than_or_equal = a >= b
+equal = a == b
+not_equal = a != b
+
+print("b < a: ",less_than)
+print("a > b: ",greater_than)
+print("b <= a: ",less_than_or_equal)
+print("a >= b: ",greater_than_or_equal)
+print("a == b: ",equal)
+print("a != b: ",not_equal)
