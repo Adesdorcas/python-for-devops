b3e0288336f0f45692209012615b04c2b59a3caf
DORCAS ADEBAYO <adesdorcas@gmail.com>
2024-02-28 04:19:56 +0000

    changes name of result

---
+++ Day-06/02-Assignment/02-Answers/logical_task.py
@@ -0,0 +1,15 @@
+# Define boolean variables
+x = True
+y = False
+
+# Perform logical operations
+and_result = x and y
+or_result = x or y
+not_result_x = not x
+not_result_y = not y
+
+# Print the results
+print("Result of x and y:", and_result)
+print("Result of x or y:", or_result)
+print("Result of not x:", not_result_x)
+print("Result of not y:", not_result_y)
