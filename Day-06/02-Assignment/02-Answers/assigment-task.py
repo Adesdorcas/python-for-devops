4530016f5b35209b5ed96dcf3f3ebe6216b01eea
DORCAS ADEBAYO <adesdorcas@gmail.com>
2024-02-29 09:31:47 +0000

    executed task 4

---
+++ Day-06/02-Assignment/02-Answers/assigment-task.py
@@ -0,0 +1,10 @@
+total = 10
+
+# Update the value of total using assignment operators
+total += 5  # Add 5 to total
+total -= 3  # Subtract 3 from total
+total *= 2  # Multiply total by 2
+total /= 4  # Divide total by 4
+
+# Print the final value of total
+print("Final value of total:", total)
